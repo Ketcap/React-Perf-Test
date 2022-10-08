7653995b4d4fa1cb94f3943501f26b4d58feac2b
Uğur Oruç <uoruc5@hotmail.com>
2022-10-09 01:02:52 +0200

    add: performance page

---
+++ component/ItemCard.tsx
@@ -0,0 +1,34 @@
+import { Card, Group, Text, Checkbox } from "@mantine/core";
+
+interface ItemCardProps {
+  itemIndex: number;
+  onChange: (isChecked: boolean, itemIndex: number) => void;
+  isChecked: boolean;
+}
+
+// eslint-disable-next-line react/display-name
+export const ItemCard = ({ itemIndex, onChange, isChecked }: ItemCardProps) => {
+  return (
+    <Card
+      shadow="sm"
+      p="sm"
+      radius="md"
+      withBorder
+      key={itemIndex}
+      onClick={() => onChange(!isChecked, itemIndex)}
+    >
+      <Group position="apart" mt="md" mb="xs">
+        <Text weight={500}>Item number {itemIndex}</Text>
+        <Checkbox
+          checked={isChecked}
+          onChange={() => onChange(!isChecked, itemIndex)}
+        />
+      </Group>
+
+      <Text size="sm" color="dimmed">
+        With Fjord Tours you can explore more of the magical fjord landscapes
+        with tours and activities on and around the fjords of Norway
+      </Text>
+    </Card>
+  );
+};
