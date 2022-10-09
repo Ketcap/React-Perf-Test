2afb5536c018ea5af94e2c34914a9dd1ca39a191
Uğur Oruç <uoruc5@hotmail.com>
2022-10-09 02:03:29 +0200

    add: more shitter test

--- __tests__/example.test.jsx
+++ __tests__/example.test.jsx
@@ -1,5 +1,5 @@
-import { render, screen } from "@testing-library/react";
-import React from 'react';
+import { render, screen,act } from "@testing-library/react";
+import React, { Profiler } from 'react';
 import Home from "../pages/main";
 import "@testing-library/jest-dom";
 
@@ -11,5 +11,18 @@ describe("Main Test", () => {
 
     expect(cards.length).toBe(500);
   });
+
+  it("re-render under 16ms to have 60fps",()=>{
+    let timers = [] ;
+    const page = render(<Profiler id="Home" onRender={(_,phase,duration)=> timers = [phase,duration]}><Home/></Profiler>)
+
+    let cards = screen.getAllByTestId("card-item");
+
+    act(()=> cards[0].click());
+    
+    page.rerender();
+
+    expect(timers[1]).toBeLessThan(16);
+  })
 
 });
